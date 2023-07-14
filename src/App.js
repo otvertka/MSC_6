b2f2c6bba5fd1f1358cc09020d79372991fb74dc
otvertka <otvertka123@ukr.net>
2023-07-14 15:14:53 +0300

    cart-context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = (event) => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = (event) => {
+    setCartIsShown(false);
+  };
+
   return (
-    <>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
